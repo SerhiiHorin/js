f0f2da813e66579b4fb2f8f4a4820e2c23ec170a
SerhiiHorin <mr.bladeee@gmail.com>
2016-07-02 11:11:33 +0000

    add new file

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a,b){
+    return a + b;
+}
+var sum1 = 100;
+var sum2 = 200;
+var result = add(100, 200);
+alert(result);
+
+
+function add(a,b,c){
+    return a + b +c;
+}
+var sum1 = 3;
+var sum2 = 2;
+var sum3 = 1987;
+var result = add(sum1, sum2, sum3);
+alert(result);
+
+
+function add(num1, num2,num3){
+    return num1 + num2 + num3;
+}
+var sum1 = Number(prompt('Enter day') );
+var sum2 = 2;
+var sum3 = 1987;
+var result = add(sum1, sum2, sum3);
+alert(result);
+
+function checkAge(age){
+    if (age >=18) {
+        return 'Adult';
+    } else {
+        return 'Child';
+    }
+}
+
+var result = Math.sqrt(4);
+alert(result); /* llkz*/
